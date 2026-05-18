53dbf9263dd68f7136f8e565ce2e34644ab4ca2b
smRid <omorsk911@gmail.com>
2026-05-18 15:28:05 +0600

    add data fetching and table component

---
+++ src/app/lib/data.js
@@ -0,0 +1,5 @@
+export const getUsers = async() =>{
+    const res = await fetch('http://localhost:5000/users');
+    const data = await res.json();
+    return data;
+}
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { getUsers } from '../lib/data';
+import UsersTable from '../components/UsersTable';
 
-const UsersPage = () => {
+const UsersPage = async () => {
+    const users = await getUsers();
+
+
     return (
         <div>
-            <h2>User Management</h2>
+            <h2>User Management: {users.length}</h2>
+            <UsersTable users = {users} > </UsersTable>
         </div>
     );
 };
