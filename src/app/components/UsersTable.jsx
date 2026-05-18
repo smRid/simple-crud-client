b0783575012cf00a7bbfcf98abf96e193bc654ff
smRid <omorsk911@gmail.com>
2026-05-18 15:30:35 +0600

    implement user management features including table display, deletion action, and add user modal

--- src/app/components/UsersTable.jsx
+++ src/app/components/UsersTable.jsx
@@ -1,8 +1,15 @@
-import { Button, Table } from '@heroui/react';
+'use client';
+import { AlertDialog, Button, Table } from '@heroui/react';
 import Link from 'next/link';
 import React from 'react';
 
-const UsersTable = ({ users }) => {
+
+const UsersTable = ({ users, deleteUserAction }) => {
+
+    const handleDelete = async(userId) => {
+        await deleteUserAction(userId);
+    }
+
     return (
         <Table>
             <Table.ScrollContainer>
@@ -26,7 +33,35 @@ const UsersTable = ({ users }) => {
                                     <Link href={`/users/${user._id}`} >
                                         <Button variant="outline">Edit</Button>
                                     </Link>
-                                    <Button variant="danger">Delete</Button>
+                                    <AlertDialog>
+                                        <Button variant="danger">Delete</Button>
+                                        <AlertDialog.Backdrop>
+                                            <AlertDialog.Container>
+                                                <AlertDialog.Dialog className="sm:max-w-[400px]">
+                                                    <AlertDialog.CloseTrigger />
+                                                    <AlertDialog.Header>
+                                                        <AlertDialog.Icon status="danger" />
+                                                        <AlertDialog.Heading>Delete User permanently?</AlertDialog.Heading>
+                                                    </AlertDialog.Header>
+                                                    <AlertDialog.Body>
+                                                        <p>
+                                                            This will permanently delete <strong>{user.name}</strong> and all of its
+                                                            data. This action cannot be undone.
+                                                        </p>
+                                                    </AlertDialog.Body>
+                                                    <AlertDialog.Footer>
+                                                        <Button slot="close" variant="tertiary">
+                                                            Cancel
+                                                        </Button>
+                                                        <Button slot="close"
+                                                            onClick={() => handleDelete(user._id)} variant="danger">
+                                                            Confirm Delete
+                                                        </Button>
+                                                    </AlertDialog.Footer>
+                                                </AlertDialog.Dialog>
+                                            </AlertDialog.Container>
+                                        </AlertDialog.Backdrop>
+                                    </AlertDialog>
                                 </Table.Cell>
                             </Table.Row>)
                         }
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { getUsers } from '../lib/data';
 import UsersTable from '../components/UsersTable';
+import { deleteUser } from '../lib/actions';
+import AddUserModal from '../components/AddUserModal';
 
 const UsersPage = async () => {
     const users = await getUsers();
@@ -8,8 +10,11 @@ const UsersPage = async () => {
 
     return (
         <div>
-            <h2>User Management: {users.length}</h2>
-            <UsersTable users = {users} > </UsersTable>
+            <div className='flex justify-between'>
+                <h2>User Management: {users.length}</h2>
+                <AddUserModal></AddUserModal>
+            </div>
+            <UsersTable users={users} deleteUserAction={deleteUser} > </UsersTable>
         </div>
     );
 };
